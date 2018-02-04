9885719f0f7ebf6cec4d3c7a5b4b001abd12b547
Дмитрий <shevtsovdiman@gmail.com>
2018-02-04 22:18:33 +0300

    Добавил таблицу и сущность

---
+++ src/entities/dictionary/Job.java
@@ -0,0 +1,65 @@
+package entities.dictionary;
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+import java.math.BigDecimal;
+
+@DatabaseTable(tableName = "jobs")
+public class Job {
+
+    @DatabaseField(generatedId = true)
+    private int id;
+    @DatabaseField
+    private String name;
+    @DatabaseField
+    private BigDecimal price;
+    @DatabaseField
+    private String category;
+
+    public Job() {
+    }
+
+    public Job(String name, BigDecimal price, String category) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+
+    public Job(String name, String category) {
+        this.name = name;
+        this.category = category;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
